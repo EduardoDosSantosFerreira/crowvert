f1235dae6fb9ac9cb0c82793ccfd59ed01deb201
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-07-29 16:04:33 -0300

    Atualiza instruções de uso do PyInstaller, adiciona novas funções de conversão no módulo e remove arquivo compilado desnecessário.

--- python/__init__.py
+++ python/__init__.py
@@ -1,11 +1,19 @@
-from .txt_to_docx import txt_to_docx
-from .docx_to_txt import docx_to_txt
+# Conversão de arquivos compactados
 from .sevenzip_to_zip import sevenzip_to_zip
 from .zip_to_sevenzip import zip_to_sevenzip
+
+# Conversão de documentos
+from .csv_to_xlsx import csv_to_xlsx
 from .docx_to_pdf import docx_to_pdf
+from .docx_to_txt import docx_to_txt
 from .pdf_to_docx import pdf_to_docx
-from .svg_to_png import svg_to_png
-from .pdf_to_png import pdf_to_png
 from .rtf_to_docx import rtf_to_docx
-from python.png_to_pdf import png_to_pdf
 from .rtf_to_pdf import rtf_to_pdf
+from .txt_to_docx import txt_to_docx
+
+# Conversão de imagens
+from .jpg_to_png import jpg_to_png
+from .pdf_to_png import pdf_to_png
+from .png_to_ico import png_to_ico
+from .png_to_pdf import png_to_pdf
+from .svg_to_png import svg_to_png
